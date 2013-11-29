a3a9bc412fab7f2f8a6aa47035e3abda83023f9d
kowcop <kowcop+@gmail.com>
2013-11-29 14:06:35 +1100

    large update

--- prebuilt/addon.d/70-gapps.sh
+++ prebuilt/addon.d/70-gapps.sh
@@ -6,93 +6,45 @@
 
 list_files() {
 cat <<EOF
-app/Books.apk
-app/CalendarGoogle.apk
-app/Drive.apk
-app/GenieWidget.apk
-app/Gmail2.apk
+app/ChromeBookmarksSyncAdapter.apk
+app/GalleryGoogle.apk
+app/GoogleCalendarSyncAdapter.apk
+app/GoogleCamera.apk
 app/GoogleContactsSyncAdapter.apk
-app/GoogleEars.apk
-app/GoogleEarth.apk
 app/GoogleHome.apk
-app/GoogleTTS.apk
-app/Hangouts.apk
-app/Keep.apk
-app/LatinImeGoogle.apk
-app/Magazines.apk
-app/Maps.apk
 app/MediaUploader.apk
-app/Music2.apk
-app/PlayGames.apk
-app/PlusOne.apk
-app/QuickOffice.apk
-app/Street.apk
-app/Videos.apk
-app/YouTube.apk
-etc/g.prop
+etc/permissions/com.google.android.camera2.xml
 etc/permissions/com.google.android.maps.xml
 etc/permissions/com.google.android.media.effects.xml
 etc/permissions/com.google.widevine.software.drm.xml
 etc/permissions/features.xml
 etc/preferred-apps/google.xml
+framework/com.google.android.camera2.jar
 framework/com.google.android.maps.jar
 framework/com.google.android.media.effects.jar
 framework/com.google.widevine.software.drm.jar
 lib/libAppDataSearch.so
-lib/libdocscanner_image-v7a.so
-lib/libdocsimageutils.so
-lib/libearthandroid.so
-lib/libearthmobile.so
-lib/libfacetracker.so
-lib/libfilterframework_jni.so
-lib/libfilterpack_facedetect.so
-lib/libfrsdk.so
 lib/libgames_rtmp_jni.so
+lib/libgcam.so
+lib/libgcam_swig_jni.so
 lib/libgoogle_recognizer_jni_l.so
+lib/libjni_eglfence.so
+lib/libjni_filtershow_filters.so
 lib/libjni_latinime.so
-lib/libjni_latinimegoogle.so
-lib/libjni_t13n_shared_engine.so
-lib/liblinearalloc.so
-lib/libmoviemaker-jni.so
-lib/libndk1.so
-lib/libnetjni.so
-lib/libocrclient.so
-lib/libpatts_engine_jni_api.so
-lib/libplus_jni_v8.so
-lib/librectifier-v7a.so
-lib/librs.antblur.so
-lib/librs.antblur_constant.so
-lib/librs.antblur_drama.so
-lib/librs.drama.so
-lib/librs.film_base.so
-lib/librs.fixedframe.so
-lib/librs.grey.so
-lib/librs.image_wrapper.so
-lib/librs.retrolux.so
-lib/librsjni.so
-lib/libRSSupport.so
-lib/libspeexwrapper.so
-lib/libstlport_shared.so
+lib/libjni_mosaic.so
+lib/liblightcycle.so
+lib/libnativehelper_compat.so
 lib/libvcdecoder_jni.so
-lib/libvideochat_jni.so
-lib/libvorbisencoder.so
-lib/libwebp_android.so
-lib/libwebrtc_audio_coding.so
-lib/libwebrtc_audio_preprocessing.so
-lib/libWVphoneAPI.so
-priv-app/CalendarProvider.apk
+priv-app/GmsCore.apk
 priv-app/GoogleBackupTransport.apk
 priv-app/GoogleFeedback.apk
 priv-app/GoogleLoginService.apk
+priv-app/GoogleNow.apk
+priv-app/GoogleOneTimeInitializer.apk
 priv-app/GooglePartnerSetup.apk
 priv-app/GoogleServicesFramework.apk
-priv-app/OneTimeInitializer.apk
 priv-app/Phonesky.apk
-priv-app/PrebuiltGmsCore.apk
 priv-app/SetupWizard.apk
-priv-app/talkback.apk
-priv-app/Velvet.apk
-priv-app/Wallet.apk
 usr/srec/en-US/c_fst
 usr/srec/en-US/clg
 usr/srec/en-US/commands.abnf
@@ -148,17 +100,23 @@ case "$1" in
     # Stub
   ;;
   post-restore)
-	# Remove the Pico TTS app 
-	rm -f /system/app/PicoTts.apk
-  
-    # Remove the AOSP Stock Launcher after restore
-    rm -f /system/priv-app/Launcher2.apk
-    rm -f /system/priv-app/Launcher3.apk   
-    rm -f /system/app/Launcher2.apk
-    rm -f /system/app/Launcher3.apk   
-
-    # Remove the AOSP Keyboard after restore - NOT on Mini Builds 
-	rm -f system/app/LatinIME.apk
-    rm -f /system/lib/libjni_latinime.so
-;;
+   rm -f /system/app/Camera2.apk
+   rm -f /system/app/Gallery2.apk
+   rm -f /system/app/Launcher2.apk
+   rm -f /system/app/Launcher3.apk
+   rm -f /system/app/QuickSearchBox.apk
+   rm -f /system/app/Velvet.apk
+   rm -f /system/app/Vending.apk
+   rm -f /system/app/BrowserProviderProxy.apk
+   rm -f /system/app/PartnerBookmarksProvider.apk
+   rm -f /system/priv-app/Camera2.apk
+   rm -f /system/priv-app/Gallery2.apk
+   rm -f /system/priv-app/Launcher2.apk
+   rm -f /system/priv-app/Launcher3.apk
+   rm -f /system/priv-app/QuickSearchBox.apk
+   rm -f /system/priv-app/Velvet.apk
+   rm -f /system/priv-app/Vending.apk
+   rm -f /system/priv-app/BrowserProviderProxy.apk
+   rm -f /system/priv-app/PartnerBookmarksProvider.apk 
+  ;;
 esac
